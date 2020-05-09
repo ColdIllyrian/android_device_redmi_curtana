4995530cd353648d030d2a2746a832f9aef695c7
DarkDroidDev <darkdroid.dev@gmail.com>
2020-05-09 22:22:50 +0200

    curtana: rootdir: Import ramdisk from V11.0.1.0.QJWMIXM
    * qssi-user 10 QKQ1.191215.002 V11.0.1.0.QJWMIXM release-keys

--- device.mk
+++ device.mk
@@ -49,6 +49,34 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.mi.usb.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    init.mi_thermald.rc \
+    init.msm.usb.configfs.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    fstab.qcom \
+    ueventd.rc
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
