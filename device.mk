f0ee49a4e2db63cdba6fde7133c48580d6888d8a
PIPIPIG233666 <2212848813@qq.com>
2020-05-17 01:17:57 +0200

    curtana: Build vendor variant of vendor.qti.hardware.camera.device@1.0.so
     * It should be a implicitly built QTI camera shared library.
     * Somehow it's not been built right now.
    
    Change-Id: Ic015e09e0a31d1f64970908b911111a2a99629fe

--- device.mk
+++ device.mk
@@ -98,7 +98,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libxml2
+    libxml2 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
     Snap
