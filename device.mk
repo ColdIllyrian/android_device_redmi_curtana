42a3e96199b7d3d85890da9844542eb621419a90
PIPIPIG233666 <pig.priv@gmail.com>
2020-05-18 09:03:13 +0200

    curtana: build 32bit audio HIDLs

--- device.mk
+++ device.mk
@@ -69,14 +69,14 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0 \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@5.0-impl:32 \
     android.hardware.audio.common@5.0 \
     android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@5.0 \
-    android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     android.hardware.soundtrigger@2.2-service
 
 PRODUCT_COPY_FILES += \
