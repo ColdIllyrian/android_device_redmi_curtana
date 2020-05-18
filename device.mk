10f41a4eb713ebad552e9c22d5d5d6911bad4c69
Wang Han <416810799@qq.com>
2020-05-18 03:11:52 +0200

    curtana: build thermal HIDL and HAL from source

--- device.mk
+++ device.mk
@@ -374,6 +374,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.atoll
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
